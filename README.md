f677b52427c51a5920d295276ae9bec2aa3b595d
Coder_Takshil <84800625+Coder-Takshil@users.noreply.github.com>
2021-06-11 23:47:23 +0530

    Initialize add-code-of-conduct-1

---
+++ README.md
@@ -0,0 +1,17 @@
+# scrcpy
+License of scrcpy goes to Genymotion reach them out at https://github.com/Genymobile/scrcpy
+This is the automate app for the mobile emulator scrcpy  
+For Now I only support Windows a linux edition will be 
+made soon by me Just install the Folder 
+(U can trust me I am not gonna upload any virus...) 
+Open the folder If u r a 64 bit user : check for : Scrcpy_automater_for_users64bit.exe, 
+Double click it and give admin. 
+After then U know what u have to do.
+If u r a 32 bit user : check for : scrcpy_for_users32bit.bat (Sorry i couldnt make an exe for 32 bit coz i am a 64 bit user),
+Double click it and give admin. After then U know what  u have to do.
+Want more help reach me out at the issues column. 
+Just install the folder from : https://mega.nz/folder/wo42iJ4T#VjWMc_79847vucr90aFxKQ 
+Or if u r not able to install, Install scrcpy from : scrcpy-win64-v1.17.zip  
+extract it and copy : Scrcpy_automater_for_users64bit.exe and scrcpy_for_users32bit.bat 
+to the extracted folder of scrcpy-win64-v1.17 and perform the above steps.
+Sorry for English errors typos they're.
---
+++ scrcpy_for_users32bit.bat
@@ -0,0 +1,240 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDZRSwOLAE+1EbsQ5+n//NaErVsYRqw2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDZRSwOLAE+1EbsQ5+n//Na0oV8eRPsDS4zJ2aSXH+EG/krpdIIoxEVcl9kzGQhdbAH9Og09vSBHrmHl
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+:: BatchGotAdmin
+
+:-------------------------------------
+
+REM  --> Check for permissions
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+	
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+	
+    "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:--------------------------------------
+:start
+@echo off
+set /P c=Do you want to start scrcpy?[Y/N]?  
+if /I "%c%" EQU "Y" goto :yes
+if /I "%c%" EQU "N" goto :no
+goto :start
+{
+:yes
+
+echo scrcpy is an open-source mobile emulator based on genymotion
+echo This script is written by Takshil Mistry to automate scrcpy.
+timeout 5
+echo Enter "1", "2", "3", "4", "5" for definitions '400', '1024', '1200', '1500', '1900' respectively.
+echo enter "d" for the default definition. 
+echo Default definition doesn't work properly on some devices.
+echo Always try the others first.
+echo Meaning of different definitions:
+echo 400 is the smallest definition and will show a 184x400 device emulation
+echo 1024 is the low quality definition and will show a 472x1024 device emulation
+echo 1200 is a good quality definition and will show a 552x1200 device emulation
+echo 1500 is a better quality definition and will show a 688x1496 device emulation
+echo 1900 is a best quality definition and will show a 880x1896 device emulation
+timeout 20
+goto :yes
+set /P c=Enter your definition number : [1, 2, 3, 4, 5, d] 
+if /I "%c%" EQU "1" goto :1
+if /I "%c%" EQU "2" goto :2
+if /I "%c%" EQU "3" goto :3
+if /I "%c%" EQU "4" goto :4
+if /I "%c%" EQU "5" goto :5
+if /I "%c%" EQU "d" goto :d
+{
+:1
+scrcpy -m 400
+
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy -m 400
+echo Thank you for using scrcpy.....
+}
+{
+:e
+
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+{
+:2
+scrcpy -m 1024
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy -m 1024
+echo Thank you for using scrcpy.....
+}
+{
+:e
+
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+{
+:3
+
+scrcpy -m 1200
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy -m 1200
+echo Thank you for using scrcpy.....
+}
+{
+:e
+
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+{
+:4
+
+scrcpy -m 1500
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy -m 1500
+echo Thank you for using scrcpy.....
+}
+{
+:e
+
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+{
+:5
+
+scrcpy -m 1900
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy -m 1900
+echo Thank you for using scrcpy.....
+}
+{
+:e
+
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+{
+:d
+
+scrcpy
+set /P c=Did scrcpy crashed on you removing your connected device?[Y/N] 
+if /I "%c%" EQU "Y" goto :s
+if /I "%c%" EQU "N" goto :e
+{
+:s
+echo wait for 5 seconds we will restart it for you.
+timeout 5
+scrcpy
+echo Thank you for using scrcpy.....
+}
+{
+:e
+echo Thank you for using scrcpy.....
+pause && exit
+}
+}
+}
+{
+:no
+set /P c=Do you really want to exit scrcpy?[Y/N]?  
+if /I "%c%" EQU "Y" goto :y
+if /I "%c%" EQU "N" goto :n
+{
+:y
+pause && exit
+}
+{
+:n
+cd C:\Users\Meggi\Downloads\scrcpy-win64-v1.17
+echo Starting scrcpy at the max definition.....
+scrcpy -m 1900
+echo Thank You for using scrcpy.
+pause && exit
+}
